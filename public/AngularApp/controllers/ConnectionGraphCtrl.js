da8cc634970c8d4e9a6df72c680ea52000e37b83
Henry Liu <dohbutz@gmail.com>
2013-10-07 16:12:05 -0700

    debuging

--- public/AngularApp/controllers/ConnectionGraphCtrl.js
+++ public/AngularApp/controllers/ConnectionGraphCtrl.js
@@ -8,6 +8,7 @@ angular.module("graphapp").controller("ConnectionGraphCtrl",function($scope, $q,
 	console.log($routeParams.sourceUserId);
 	console.log($routeParams.targetUserId);
 	*/
+	console.log("HERE WE GO!");
 	$scope.desiredUserId = salesforceconnections.getSourceUsers();//'00530000008B7WiAAK';
 	$scope.myUserId = salesforceconnections.getTargetUsers();
 	console.log('target: '+ $scope.desiredUserId + ' ,source: '+ $scope.myUserId);
